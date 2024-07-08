b1c96dd2b5b4edf9b1f442828edb6a8c22784343
Shubham <rockycooldude97@gmail.com>
2024-07-08 23:37:39 +0530

    addition of 2 number and empty string check

--- AddNumber/src/main/java/com/addnumber/AddNumber.java
+++ AddNumber/src/main/java/com/addnumber/AddNumber.java
@@ -1,8 +1,17 @@
 package com.addnumber;
 public class AddNumber {
 	
-	public int addNumber(String number) {
-		return 0;
+	public static int addNumber(String number) {
+		if(number.length()==0) {
+			return 0;
+		}
+		String numArr[]=number.split(",");
+		int sum=0;
+		
+		for(int index=0;index<numArr.length;index++) {
+			sum=sum+Integer.parseInt(numArr[index]);
+		}
+		return sum;
 	}
 
 }
--- AddNumber/src/test/java/com/addnumber/test/AddNumberTest.java
+++ AddNumber/src/test/java/com/addnumber/test/AddNumberTest.java
@@ -9,8 +9,17 @@ import com.addnumber.AddNumber;
 public class AddNumberTest {
 
 	@Test
-	public void testSum() {
-		
+	public void testBaisc() {
+		AddNumber add= new AddNumber();
+		int output=add.addNumber("1,2");
+		assertEquals(3, output);
+	}
+	
+	@Test
+	public void testEmpty() {
+		AddNumber add= new AddNumber();
+		int output=add.addNumber("");
+		assertEquals(0, output);
 	}
 
 }
