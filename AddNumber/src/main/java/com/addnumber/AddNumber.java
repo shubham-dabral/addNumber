7eb81163842fb31649e524c653d4c741191a9fa4
Shubham <rockycooldude97@gmail.com>
2024-07-09 18:14:58 +0530

    delimiter changes

--- AddNumber/src/main/java/com/addnumber/AddNumber.java
+++ AddNumber/src/main/java/com/addnumber/AddNumber.java
@@ -1,20 +1,42 @@
 package com.addnumber;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class AddNumber {
-	
+
 	
 	public int addNumber(String number) {
+		
 		if(number.length()==0) {
 			return 0;
 		}
-		else if (number.contains("\n")) {
+		
+		String regex = "[!\"#$%&'*+,./:;=?@^_`|~-]+";
+		Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(number);
+        
+        StringBuilder sb = new StringBuilder();
+        while (matcher.find()) { 
+            matcher.appendReplacement(sb, ","); 
+        } 
+        matcher.appendTail(sb); 
+
+		number=sb.toString();
+		
+		if (number.contains("\n")) {
 			number=number.replace("\n", ",");
 		}
+		
 		String numArr[]=number.split(",");
+		
 		int sum=0;
 		
 		for(int index=0;index<numArr.length;index++) {
-			sum=sum+Integer.parseInt(numArr[index].trim());
+			if(!numArr[index].trim().equals(""))
+				sum=sum+Integer.parseInt(numArr[index].trim());
 		}
+		
 		return sum;
 	}
 
--- AddNumber/src/test/java/com/addnumber/test/AddNumberTest.java
+++ AddNumber/src/test/java/com/addnumber/test/AddNumberTest.java
@@ -29,15 +29,26 @@ public class AddNumberTest {
 
 	@Test
 	public void testWithNextLine() {
-		int output=add.addNumber("1\n2,3");
-		assertEquals(6, output);
+		int output=add.addNumber("1\n2,3,4\n");
+		assertEquals(10, output);
 		
 	}
+	
 	@Test
 	public void testWithTab() {
 		int output=add.addNumber("1,2\t,3");
 		assertEquals(6, output);
 		
 	}
+	
+	@Test
+	public void testDelimiter() {
+//		int output=add.addNumber("//;\n1;2");
+		int output=add.addNumber("//1\n2;\n1;2");
+//		assertEquals(3, output);
+		assertEquals(6, output);
+	}
+	
+	
 }
 
